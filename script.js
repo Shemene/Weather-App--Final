8da1cc4edf35149cb565ff474e0fe65bb7deeffa
Shem <shemenekok@gmail.com>
2023-11-17 12:24:25 +0200

    Calling data from Api

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-//create a search engine
+function refreshWeather(response) {
+  console.log(response.data);
+}
 
+function searchCity(city) {
+  let apiKey = "91t68d1a23a3153b0a155oba25d504df";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+//create a search engine
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
+//replace the city element with search
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
-
