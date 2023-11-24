77d01faf27caafa03e7af3f3918fd1641b5729e8
Shem <shemenekok@gmail.com>
2023-11-24 10:42:41 +0200

    Injected HTML forecast Html with Javascript

--- script.js
+++ script.js
@@ -59,3 +59,18 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Cape Town");
+
+let forecast = document.querySelector("#forecast");
+
+forecast.innerHTML = `<div class="weather-forecast-date">Tue</div>
+
+              <img
+                src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
+                alt=""
+                width="36"
+              />
+              <div class="weather-forecast-temperatures">
+                <span class="weather-forecast-temperature-max">18°</span
+                ><span class="weather-forecast-temperature-min">12°</span>
+              </div> 
+            </div>`;
